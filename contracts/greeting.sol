8677c03b92419c182019308fb441dd3724249552
Wipanee Khamluewong <wipanee_k@BKC.CO.TH>
2018-11-06 14:32:06 +0700

    ประกาศ contract และกำหนดค่าให้ message ตอน contract ถูกสร้าง

--- contracts/greeting.sol
+++ contracts/greeting.sol
@@ -1,2 +1,13 @@
 //solium-disable linebreak-style
 pragma solidity ^0.4.18;
+
+contract Greetings {
+    string message;
+
+    constructor() public {
+        message = "OK na ka.";
+    }
+
+    
+
+}
